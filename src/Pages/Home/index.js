c302775478f864e8b9fd0597598c1f59bf3c972d
dhiyo7 <dhiyoreksa12@gmail.com>
2020-11-29 03:06:22 +0700

    3 - Categories

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 import Carousel from "../../Components/Carousel";
+import Categories from "../../Components/Categories";
 
 export default class Home extends Component {
   render() {
-    return <Carousel />;
+    return (
+      <>
+        <Carousel />
+        <Categories />
+      </>
+    );
   }
 }
